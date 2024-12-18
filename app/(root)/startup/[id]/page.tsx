e32e61bb57af6596b29a20aa62c1fe3f1d59804c
iam-prabha <dev.vpcode@gmail.com>
2024-12-18 13:30:08 +0530

    issue fix it

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -21,15 +21,12 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const [post, { select: editorPosts }] = await Promise.all([
+  const [post, editorPosts] = await Promise.all([
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
     client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-      slug: "editor-other-picks",
+      slug: "editor-choice-picks",
     }),
   ]);
-
-  console.log("post: ", post);
-  console.log("editorPosts: ", editorPosts);
 
   if (!post) return notFound();
 
